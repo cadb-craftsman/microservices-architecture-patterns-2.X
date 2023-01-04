1579d921dc4a0cb59ef2f065ae36b3f5f85c04a1
carlos <cadb.craftsman@gmail.com>
2023-01-04 17:31:57 +0100

    update main class with @Typehint to solve the eureka conection server error.

--- springnative-backend-reactservice/src/main/java/com/craftsman/reactservice/ReactServiceApp.java
+++ springnative-backend-reactservice/src/main/java/com/craftsman/reactservice/ReactServiceApp.java
@@ -1,15 +1,22 @@
 package com.craftsman.reactservice;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInfo;
+import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 import io.r2dbc.spi.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 
+@TypeHint(types = InstanceInfo.class, typeNames = "com.netflix.appinfo.InstanceInfo$PortWrapper")
+@TypeHint(types = Application.class, typeNames = "com.netflix.discovery.shared.Application")
+@TypeHint(types = MyDataCenterInfo.class, typeNames = "com.netflix.appinfo.MyDataCenterInfo")
 @Slf4j
 @SpringBootApplication
 public class ReactServiceApp {
--- springnative-backend-restservice/src/main/java/com/craftsman/restservice/RestServiceApp.java
+++ springnative-backend-restservice/src/main/java/com/craftsman/restservice/RestServiceApp.java
@@ -1,8 +1,15 @@
 package com.craftsman.restservice;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInfo;
+import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
+@TypeHint(types = InstanceInfo.class, typeNames = "com.netflix.appinfo.InstanceInfo$PortWrapper")
+@TypeHint(types = Application.class, typeNames = "com.netflix.discovery.shared.Application")
+@TypeHint(types = MyDataCenterInfo.class, typeNames = "com.netflix.appinfo.MyDataCenterInfo")
 @SpringBootApplication
 public class RestServiceApp {
 
