338df3e93c5f8e5eb1db371ecdd74d10c18885fc
carlos <cadb.craftsman@gmail.com>
2023-01-16 17:47:55 +0100

    rename projects and includes function project.

---
+++ springnative-mybatisreactservice-eureka/src/main/java/com/craftsman/restservice/domain/mappers/RestServiceFactoryMapper.java
@@ -0,0 +1,15 @@
+package com.craftsman.restservice.domain.mappers;
+
+import org.mapstruct.Mapper;
+
+import com.craftsman.restservice.domain.dto.RestServiceDto;
+import com.craftsman.restservice.domain.model.RestServiceBean;
+
+@Mapper
+public interface RestServiceFactoryMapper {
+	
+	RestServiceBean mapToRestServiceEntity(RestServiceDto in);
+	
+	RestServiceDto mapToRestServiceDto(RestServiceBean in);
+
+}
